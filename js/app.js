3808e9f15ff11ea00e24be50f64b7f764754ba82
mcales <calesme@guilford.edu>
2016-05-29 19:07:50 -0400

    quiz commit

--- js/app.js
+++ js/app.js
@@ -5,31 +5,31 @@ var defaultAnswer = 9;
 //Array of Question Objects
 var questions = [{
 	ques : "Which field of physics is most associated with Isaac Newton?",
-	ans : ["Mechanics       ", "Thermodynamics", "Electricity & Magnetism", "Quantum Mechanics"],
+	ans : ["Mechanics                  ", "Thermodynamics               ", "Electricity & Magnetism", "Quantum Mechanics"],
 	correct : 0,
 	desc : "Mechanics is also commonly called Newtonian mechanics. Newton published the Principia which provided the framework for classical physics",
 	img: 'images/newton.jpg'
 },
 {	ques : "For what discovery did Albert Einstein recieve the Nobel prize?",
-	ans : ["Relativity       ", "Quarks", "Photoelectric Effect", "Brownian Motion"],
+	ans : ["Relativity                     ", "Quarks              ", "Photoelectric Effect", "Brownian Motion"],
 	correct : 2,
 	desc : "Albert Einstein recieved the Nobel Prize for his description of the Photoelectric Effect in 1921.",
 	img: 'images/einstein.jpg'
 },
 {	ques : "Which scientist is credited with finding the first photographic evidence of relativity?",
-	ans : ["Arthur Eddington", "Albert Einstein", "Neils Bohr", "Stephen Hawking"],
+	ans : ["Arthur Eddington", "Albert Einstein", "Neils Bohr         ", "Stephen Hawking       "],
 	correct : "0",
 	desc :"Arthur Eddington is credited with showing evidence of relativistic lensing in an image of a solar eclipse in 1919.",
 	img: 'images/eclipse.jpg'
 },
 {	ques : "Who is considered the father of modern quantum mechanics?",
-	ans : ["Neils Bohr     ", "Max Planck", "Erwin Schroedinger", "Albert Einstein"],
+	ans : ["Neils Bohr               ", "Max Planck         ", "Erwin Schroedinger", "Albert Einstein         "],
 	correct : 1,
 	desc : "Max Plack's influence was instrumental in early quantum mechanical models and theory.",
 	img: 'images/atom.jpg'
 },
 {	ques : "What particle is believed to have been discovered at the LHC in Switzerland?",
-	ans : ["Higgs Boson          ", "Quarks", "Neutrino          ", "Positron"],
+	ans : ["Higgs Boson               ", "Quarks            ", "Neutrino                      ", "Positron             "],
 	correct : 0,
 	desc : "The LHC was built, in part, to find evidence that the Higgs Boson is more than just a theoretical model. Their results indicate strong evidence of the Higgs Boson.",
 	img: 'images/lhc.jpg'
